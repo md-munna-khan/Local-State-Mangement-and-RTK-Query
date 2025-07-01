9b78a7d6a7ee9eeec75540088573af6259cd43f2
md.munna <mk8761174@gmail.com>
2025-07-01 09:41:42 +0600

    23-11 Handling data mutation (POST)

--- README.md
+++ README.md
@@ -1362,3 +1362,253 @@ export default function TaskCard({ task }: IProps) {
 }
 
 ```
+## 23-11 Handling data mutation (POST)
+
+```ts 
+// Importing the necessary functions from Redux Toolkit Query
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// Creating a base API instance using createApi
+export const baseApi = createApi({
+    // Unique key for the API reducer in the Redux store
+    reducerPath: "baseApi",
+
+    // Defines the base URL for all API calls
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+
+    // Define all API endpoints here
+    endpoints: (builder) => ({
+        // Define a "getTask" endpoint for fetching tasks
+        getTask: builder.query({
+            // This query will hit the `/tasks` route (full URL: http://localhost:5000/api/tasks)
+            query: () => "/tasks"
+        }),
+        createTask: builder.mutation({
+            query: (taskData) => ({
+                url: "/tasks",          // Endpoint to which the request will be sent
+                method: "POST",         // HTTP method used for the request
+                body: taskData          // Request payload (sent in the body of the POST)
+            })
+        })
+    })
+})
+
+// made an automatic hook 
+export const { useGetTaskQuery, useCreateTaskMutation } = baseApi
+
+```
+
+- When we do query it will return a object and we will destructure the object 
+
+
+```tsx
+const { data, isLoading, isError } = useGetTaskQuery(undefined)
+```
+
+- When We do mutation it returns an array 
+
+```tsx 
+const [createTask, { data, isLoading, isError }] = useCreateTaskMutation()
+```
+- AddTaskModal.tsx 
+
+```tsx
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Textarea } from "@/components/ui/textarea"
+import { cn } from "@/lib/utils"
+import { useCreateTaskMutation } from "@/redux/api/baseApi"
+import { format } from "date-fns"
+import { CalendarIcon } from "lucide-react"
+import { useState } from "react"
+
+import { useForm, type FieldValues, type SubmitHandler } from "react-hook-form"
+
+
+
+export function AddTaskModal() {
+
+    // responsible for closing the modal 
+    const [open, setOpen] = useState(false)
+
+    // this connects with the hook form 
+    const form = useForm()
+
+
+    const [createTask, { data, isLoading, isError }] = useCreateTaskMutation()
+
+    console.log({ "Data Before Function": data, isLoading, isError })
+
+
+
+    // 
+    const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+        const taskDta = {
+            ...data,
+            isCompleted: false
+        }
+
+        const res = await createTask(taskDta)
+        console.log("Data Inside Function", res)
+        // responsible for closing the modal 
+
+
+        setOpen(false)
+        // responsible for resetting the form 
+        form.reset()
+    }
+
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <form>
+                <DialogTrigger asChild>
+                    <Button >Add Task</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogDescription className="sr-only">Fill up This task Form to add task</DialogDescription>
+                    {/* sr-only means only the screen reader can read but this will not be visible. */}
+                    <DialogHeader>
+                        <DialogTitle>Add Task</DialogTitle>
+                    </DialogHeader>
+                    {/* changed the form */}
+                    <Form {...form}>
+                        {/* form.handleSubmit → used to handle form submission */}
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <FormField
+                                control={form.control}
+                                name="title"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Title</FormLabel>
+                                        <FormControl>
+                                            <Input  {...field} value={field.value || ""} />
+                                            {/* Input: the actual input box. value={field.value || ""} ensures it’s never undefined. */}
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                // form.control → used in every field to link it to the form
+                                name="description"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Description</FormLabel>
+                                        <FormControl>
+                                            <Textarea {...field} value={field.value || ""} />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="priority"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="mt-4">Priority</FormLabel>
+                                        {/* Bind value and onChange to react-hook-form */}
+                                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                            <FormControl>
+                                                <SelectTrigger className="w-full">
+                                                    <SelectValue placeholder="Select Priority" />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                <SelectItem value="Low">Low</SelectItem>
+                                                <SelectItem value="Medium">Medium</SelectItem>
+                                                <SelectItem value="High">High</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="assignedTo"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="mt-4">Assign User</FormLabel>
+                                        {/* Bind value and onChange to react-hook-form */}
+                                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                            <FormControl>
+                                                <SelectTrigger className="w-full">
+                                                    <SelectValue placeholder="Select User" />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                <SelectItem value="Low">Low</SelectItem>
+                                                <SelectItem value="Medium">Medium</SelectItem>
+                                                <SelectItem value="High">High</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                    </FormItem>
+                                )}
+                            />
+
+                            <FormField
+                                control={form.control}
+                                name="dueDate"
+                                render={({ field }) => (
+                                    <FormItem className="flex flex-col">
+                                        <FormLabel className="mt-4">Pick A Date</FormLabel>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <FormControl>
+                                                    <Button
+                                                        variant={"outline"}
+                                                        className={cn(
+                                                            "pl-3 text-left font-normal w-full",
+                                                            !field.value && "text-muted-foreground"
+                                                        )}
+                                                    >
+                                                        {field.value ? (
+                                                            format(field.value, "PPP")
+                                                        ) : (
+                                                            <span>Pick a date</span>
+                                                        )}
+                                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                    </Button>
+                                                </FormControl>
+                                            </PopoverTrigger>
+                                            <PopoverContent className="w-auto p-0" align="start">
+                                                <Calendar
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={field.onChange}
+                                                    // disabled={(date) =>
+                                                    //     date > new Date() || date < new Date("1900-01-01")
+                                                    // }
+                                                    captionLayout="dropdown"
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                    </FormItem>
+                                )}
+                            />
+                            <DialogFooter className="mt-4 ">
+                                <Button type="submit" className="w-full ">Save changes</Button>
+                            </DialogFooter>
+                        </form>
+
+
+                    </Form>
+                </DialogContent>
+            </form>
+        </Dialog>
+    )
+}
+
+```
--- RTK-QUERY/src/module/tasks/AddTaskModal.tsx
+++ RTK-QUERY/src/module/tasks/AddTaskModal.tsx
@@ -1,175 +1,190 @@
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { Textarea } from "@/components/ui/textarea"
-import { cn } from "@/lib/utils"
-import { useCreateTaskMutation } from "@/redux/api/baseApi"
-import { format } from "date-fns"
-import { CalendarIcon } from "lucide-react"
-import { useState } from "react"
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+import { useCreateTaskMutation } from "@/redux/api/baseApi";
 
-import { useForm, type FieldValues, type SubmitHandler } from "react-hook-form"
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { useState } from "react";
 
-
+import { useForm, type FieldValues, type SubmitHandler } from "react-hook-form";
 
 export function AddTaskModal() {
+  // responsible for closing the modal
+  const [open, setOpen] = useState(false);
 
-    // responsible for closing the modal 
-    const [open, setOpen] = useState(false)
+  // this connects with the hook form
+  const form = useForm();
 
-    // this connects with the hook form 
-    const form = useForm()
+  const [createTask, { data, isLoading, isError }] = useCreateTaskMutation();
 
+  console.log({ "Data Before Function": data, isLoading, isError });
 
-    const [createTask, { data, isLoading, isError }] = useCreateTaskMutation()
+  //
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const taskDta = {
+      ...data,
+      isCompleted: false,
+    };
 
-    console.log({ "Data Before Function": data, isLoading, isError })
+    const res = await createTask(taskDta).unwrap();
+    console.log("Data Inside Function", res);
+    // responsible for closing the modal
 
+    setOpen(false);
+    // responsible for resetting the form
+    form.reset();
+  };
 
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <form>
+        <DialogTrigger asChild>
+          <Button>Add Task</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogDescription className="sr-only">
+            Fill up This task Form to add task
+          </DialogDescription>
+          {/* sr-only means only the screen reader can read but this will not be visible. */}
+          <DialogHeader>
+            <DialogTitle>Add Task</DialogTitle>
+          </DialogHeader>
+          {/* changed the form */}
+          <Form {...form}>
+            {/* form.handleSubmit → used to handle form submission */}
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input {...field} value={field.value || ""} />
+                      {/* Input: the actual input box. value={field.value || ""} ensures it’s never undefined. */}
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                // form.control → used in every field to link it to the form
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Textarea {...field} value={field.value || ""} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="priority"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="mt-4">Priority</FormLabel>
+                    {/* Bind value and onChange to react-hook-form */}
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="Select Priority" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="low">Low</SelectItem>
+                        <SelectItem value="medium">Medium</SelectItem>
+                        <SelectItem value="high">High</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
 
-    // 
-    const onSubmit: SubmitHandler<FieldValues> = async (data) => {
-        const taskDta = {
-            ...data,
-            isCompleted: false
-        }
-
-        const res = await createTask(taskDta)
-        console.log("Data Inside Function", res)
-        // responsible for closing the modal 
-
-
-        setOpen(false)
-        // responsible for resetting the form 
-        form.reset()
-    }
-
-    return (
-        <Dialog open={open} onOpenChange={setOpen}>
-            <form>
-                <DialogTrigger asChild>
-                    <Button >Add Task</Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px]">
-                    <DialogDescription className="sr-only">Fill up This task Form to add task</DialogDescription>
-                    {/* sr-only means only the screen reader can read but this will not be visible. */}
-                    <DialogHeader>
-                        <DialogTitle>Add Task</DialogTitle>
-                    </DialogHeader>
-                    {/* changed the form */}
-                    <Form {...form}>
-                        {/* form.handleSubmit → used to handle form submission */}
-                        <form onSubmit={form.handleSubmit(onSubmit)}>
-                            <FormField
-                                control={form.control}
-                                name="title"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Title</FormLabel>
-                                        <FormControl>
-                                            <Input  {...field} value={field.value || ""} />
-                                            {/* Input: the actual input box. value={field.value || ""} ensures it’s never undefined. */}
-                                        </FormControl>
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                // form.control → used in every field to link it to the form
-                                name="description"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Description</FormLabel>
-                                        <FormControl>
-                                            <Textarea {...field} value={field.value || ""} />
-                                        </FormControl>
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="priority"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel className="mt-4">Priority</FormLabel>
-                                        {/* Bind value and onChange to react-hook-form */}
-                                        <Select onValueChange={field.onChange} defaultValue={field.value}>
-                                            <FormControl>
-                                                <SelectTrigger className="w-full">
-                                                    <SelectValue placeholder="Select Priority" />
-                                                </SelectTrigger>
-                                            </FormControl>
-                                            <SelectContent>
-                                                <SelectItem value="Low">Low</SelectItem>
-                                                <SelectItem value="Medium">Medium</SelectItem>
-                                                <SelectItem value="High">High</SelectItem>
-                                            </SelectContent>
-                                        </Select>
-                                    </FormItem>
-                                )}
-                            />
-
-                            <FormField
-                                control={form.control}
-                                name="dueDate"
-                                render={({ field }) => (
-                                    <FormItem className="flex flex-col">
-                                        <FormLabel className="mt-4">Pick A Date</FormLabel>
-                                        <Popover>
-                                            <PopoverTrigger asChild>
-                                                <FormControl>
-                                                    <Button
-                                                        variant={"outline"}
-                                                        className={cn(
-                                                            "pl-3 text-left font-normal w-full",
-                                                            !field.value && "text-muted-foreground"
-                                                        )}
-                                                    >
-                                                        {field.value ? (
-                                                            format(field.value, "PPP")
-                                                        ) : (
-                                                            <span>Pick a date</span>
-                                                        )}
-                                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                                                    </Button>
-                                                </FormControl>
-                                            </PopoverTrigger>
-                                            <PopoverContent className="w-auto p-0" align="start">
-                                                <Calendar
-                                                    mode="single"
-                                                    selected={field.value}
-                                                    onSelect={field.onChange}
-                                                    // disabled={(date) =>
-                                                    //     date > new Date() || date < new Date("1900-01-01")
-                                                    // }
-                                                    captionLayout="dropdown"
-                                                />
-                                            </PopoverContent>
-                                        </Popover>
-                                    </FormItem>
-                                )}
-                            />
-                            <DialogFooter className="mt-4 ">
-                                <Button type="submit" className="w-full ">Save changes</Button>
-                            </DialogFooter>
-                        </form>
-
-
-                    </Form>
-                </DialogContent>
+              <FormField
+                control={form.control}
+                name="dueDate"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel className="mt-4">Pick A Date</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant={"outline"}
+                            className={cn(
+                              "pl-3 text-left font-normal w-full",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            {field.value ? (
+                              format(field.value, "PPP")
+                            ) : (
+                              <span>Pick a date</span>
+                            )}
+                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          // disabled={(date) =>
+                          //     date > new Date() || date < new Date("1900-01-01")
+                          // }
+                          captionLayout="dropdown"
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </FormItem>
+                )}
+              />
+              <DialogFooter className="mt-4 ">
+                <Button type="submit" className="w-full ">
+                  Save changes
+                </Button>
+              </DialogFooter>
             </form>
-        </Dialog>
-    )
+          </Form>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
 }
